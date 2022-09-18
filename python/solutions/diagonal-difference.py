b5bef77b5dcf8cb3b0e5530e7d55f4a0d4ffdcfd
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2022-09-18 00:38:07 -0700

    Refactor & make solutions more pythonic

--- python/solutions/diagonal-difference.py
+++ python/solutions/diagonal-difference.py
@@ -1,18 +1,14 @@
-def diagonalDifference(arr):
-    def sumPrimaryDiagonal(matrix):
-        i = 0
+def diagonal_difference(arr):
+    def sum_diagonal(matrix, is_primary):
+        col = 0 if is_primary else len(matrix) - 1
+
         sum = 0
         for row in matrix:
-            sum += row[i]
-            i += 1
+            sum += row[col]
+            col += 1 if is_primary else -1
         return sum
 
-    def sumSecondaryDiagonal(matrix):
-        i = len(matrix) - 1
-        sum = 0
-        for row in matrix:
-            sum += row[i]
-            i -= 1
-        return sum
-
-    return abs(sumPrimaryDiagonal(arr) - sumSecondaryDiagonal(arr))
+    primary_sum = sum_diagonal(arr, True)
+    secondary_sum = sum_diagonal(arr, False)
+    difference = abs(primary_sum - secondary_sum)
+    return difference
